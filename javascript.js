1e6f7dc08db60393dbe1c3c60f3cb559a535a0fe
hmojtahedinia <h.mojtahedinia@gmail.com>
2019-05-25 23:08:00 -0400

    Updated index and CSS and Javascript and organized folders

--- javascript.js
+++ javascript.js
@@ -1,26 +1,27 @@
 $(document).ready(function () {
- //   $("#button").click(function () {
-        
-        
-        
-       // let apiKey = "sre3RavLD1B24q8kaxOxlfZV3FooRbgD";
-        //let hyperLink = "http://api.giphy.com/v1/gifs/search?q=" + "dog" + "&api_key=" + apiKey + "&limit=5";
-        
-        $.ajax({
-           // URL: "https://api.darksky.net/forecast/19a8df545a100d1c196c24b96afc72b8/43.6532,79.3832",
-           url: "https://api.darksky.net/forecast/cdebfdd2f00f031e2ad5aea7c2cc18f5/43.6532,-79.3832?units=si",
 
-           //url: hyperLink ,
-           type: "GET",
-           dataType: "jsonp"
-        }).then(function (data) {
-            console.log(data);
-            //$("#weather").html();
-            let t = data.currently.temperature;
-            let temp = Math.round(t); 
-            console.log(temp);
+  let div = $("#hello1");
+  setInterval(() => {
+    div.fadeOut("1000");
+    div.fadeIn("1000")
+  }, 2000);
 
-            $("#weather").append(" " + temp + "&#x2103" + "<br>" + " " + data.currently.summary);
-        }); 
-  //  }); 
+
+  $.ajax({
+    // URL: "https://api.darksky.net/forecast/19a8df545a100d1c196c24b96afc72b8/43.6532,79.3832",
+    url: "https://api.darksky.net/forecast/cdebfdd2f00f031e2ad5aea7c2cc18f5/43.6532,-79.3832?units=si",
+
+    //url: hyperLink ,
+    type: "GET",
+    dataType: "jsonp"
+  }).then(function (data) {
+    console.log(data);
+    //$("#weather").html();
+    let t = data.currently.temperature;
+    let temp = Math.round(t);
+    //console.log(temp);
+    $("#weather").append(" " + temp + "&#x2103" + "<br>" + " " + data.currently.summary);
+  });
 });
+
+
